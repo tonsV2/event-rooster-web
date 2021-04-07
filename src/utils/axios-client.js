9c2f184fcde131e4e7478edbac0b224e7046c35f
sj <sebastianthegreatful@gmail.com>
2021-04-07 11:56:59 +0700

    Fix missing typo
    Signed-off-by: sj <sebastianthegreatful@gmail.com>

--- src/utils/axios-client.js
+++ src/utils/axios-client.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    rejectUnauthorized: process.NODE_ENV === 'prod',
+    rejectUnauthorized: process.env.NODE_ENV === 'prod',
     baseURL: process.env.VUE_APP_API_BASE_URL
 })
